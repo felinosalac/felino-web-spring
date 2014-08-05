0116fb08438a574288e13f7dbe4fa83b5277854f
fsalac <fsalac@briskmobile.com>
2014-08-05 23:59:08 +0800

    added JUnit test added dependency for JUnit test

---
+++ felino-web-spring/src/test/java/com/felino/controller/BaseControllerTest.java
@@ -0,0 +1,15 @@
+package com.felino.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BaseControllerTest {
+
+	@Test
+	public void welcomeNameTest() {
+		boolean isLove = true;
+		assertEquals(isLove, true);
+	}
+
+}
